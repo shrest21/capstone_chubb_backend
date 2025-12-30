b93c9231660ff631503af44341ac5962a7892797
shrest21 <shrest21@gmail.com>
2025-12-30 11:06:56 +0530

    Added apigateway microservice registers with eureka server

---
+++ apigateway/src/main/java/com/inventoryapp/apigateway/ApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.inventoryapp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+
+}
