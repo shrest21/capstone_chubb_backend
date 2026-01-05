2e56011681680e5fde40d58cfef67d9cc4061330
shrest21 <shrest21@gmail.com>
2026-01-05 10:42:13 +0530

    Fixed issue in Cors

--- apigateway/src/main/java/com/inventoryapp/apigateway/security/SecurityConfig.java
+++ apigateway/src/main/java/com/inventoryapp/apigateway/security/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -22,16 +27,31 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeExchange(ex -> ex
                         .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/products/**").permitAll()
                         .pathMatchers("/products/**").hasRole("ADMIN")
                         .pathMatchers("/warehouse/**", "/inventory/**")
                         .hasAnyRole("ADMIN", "WAREHOUSE_MANAGER")
-                        .pathMatchers(HttpMethod.POST, "/orders/**").authenticated()
+                        .pathMatchers(HttpMethod.POST, "/orders/**").permitAll()
                         .pathMatchers(HttpMethod.GET, "/orders/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .addFilterAt(jwtAuthFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
