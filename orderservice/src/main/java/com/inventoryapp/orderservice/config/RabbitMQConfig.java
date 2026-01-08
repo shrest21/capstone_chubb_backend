e7c4bd35057f415bdef7ebd9c8abddc69f8c044e
shrest21 <shrest21@gmail.com>
2026-01-08 17:22:15 +0530

    Backend final run isses

--- orderservice/src/main/java/com/inventoryapp/orderservice/config/RabbitMQConfig.java
+++ orderservice/src/main/java/com/inventoryapp/orderservice/config/RabbitMQConfig.java
@@ -3,7 +3,7 @@ package com.inventoryapp.orderservice.config;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,14 +16,14 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public JacksonJsonMessageConverter jacksonJsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+    public Jackson2JsonMessageConverter jacksonJsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(
             ConnectionFactory connectionFactory,
-            JacksonJsonMessageConverter messageConverter) {
+            Jackson2JsonMessageConverter messageConverter) {
 
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter);
